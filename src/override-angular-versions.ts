886197a9fd445d3302ec334f859547d402e92301
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2020-10-16 01:14:59 -0300

    fix: 🐛 modify only existing version, not override entire object

--- src/override-angular-versions.ts
+++ src/override-angular-versions.ts
@@ -6,8 +6,19 @@ export function overrideAngularVersions(
   path: string
 ): PackageJsonVersion {
   const rawData = fs.readFileSync(path);
-  let packageJson: PackageJsonVersion = JSON.parse(rawData.toString());
-  packageJson = {...packageJson, ...angularVersions};
+  const packageJson: PackageJsonVersion = JSON.parse(rawData.toString());
+
+  for (const prop in angularVersions.dependencies) {
+    if (packageJson.dependencies.hasOwnProperty(prop)) {
+      packageJson.dependencies[prop] = angularVersions.dependencies[prop];
+    }
+  }
+
+  for (const prop in angularVersions.devDependencies) {
+    if (packageJson.devDependencies.hasOwnProperty(prop)) {
+      packageJson.devDependencies[prop] = angularVersions.devDependencies[prop];
+    }
+  }
 
   fs.writeFileSync(path, JSON.stringify(packageJson));
 
