e09d5f54a13f20bc1b9fb05a9d8f1940ce731fb0
Nacho Vazquez <nacho.vazquez.dev@gmail.com>
2020-10-18 00:56:34 -0300

    feat: 🎸 add support for angular 10.1.x

--- src/angular-versions.ts
+++ src/angular-versions.ts
@@ -116,5 +116,34 @@ export const versions = new Map<string, PackageJsonVersion>([
         typescript: '~3.9.5'
       }
     }
+  ],
+  [
+    '10.1.x',
+    {
+      dependencies: {
+        '@angular/animations': '~10.1.6',
+        '@angular/common': '~10.1.6',
+        '@angular/compiler': '~10.1.6',
+        '@angular/core': '~10.1.6',
+        '@angular/forms': '~10.1.6',
+        '@angular/platform-browser': '~10.1.6',
+        '@angular/platform-browser-dynamic': '~10.1.6',
+        '@angular/router': '~10.1.6',
+        rxjs: '~6.6.3',
+        tslib: '^2.0.0',
+        'zone.js': '~0.10.3'
+      },
+      devDependencies: {
+        '@angular-devkit/build-angular': '~0.1001.7',
+        '@angular-devkit/build-ng-packagr': '~0.1001.7',
+        '@angular/cli': '~10.1.7',
+        '@angular/compiler-cli': '~10.1.6',
+        '@types/node': '^12.12.62',
+        codelyzer: '^6.0.0',
+        'ts-node': '~8.3.0',
+        tslint: '~6.1.0',
+        typescript: '~3.9.7'
+      }
+    }
   ]
 ]);
