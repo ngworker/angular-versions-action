2af734c94c4e8be4b836d6e4d58a92c628c8fbbd
Nacho Vazquez <nachovc1410@gmail.com>
2021-01-21 11:52:36 -0300

    feat: 🎸 remove support for angular v11-rc

--- src/angular-versions.ts
+++ src/angular-versions.ts
@@ -237,26 +237,23 @@ export const versions = new Map<string, PackageJsonVersion>([
     '11.0.x',
     {
       dependencies: {
-        '@angular/animations': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/common': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/compiler': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/core': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/forms': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/platform-browser': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/platform-browser-dynamic':
-          '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/router': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
+        '@angular/animations': '~11.0.0',
+        '@angular/common': '~11.0.0',
+        '@angular/compiler': '~11.0.0',
+        '@angular/core': '~11.0.0',
+        '@angular/forms': '~11.0.0',
+        '@angular/platform-browser': '~11.0.0',
+        '@angular/platform-browser-dynamic': '~11.0.0',
+        '@angular/router': '~11.0.0',
         rxjs: '~6.6.0',
         tslib: '^2.0.0',
         'zone.js': '~0.10.2'
       },
       devDependencies: {
-        '@angular-devkit/build-angular':
-          '~0.1100.0 || >=0.1100.0-rc.1 <0.1100.0',
-        '@angular-devkit/build-ng-packagr':
-          '~0.1100.0 || >=0.1100.0-next.5 <0.1100.0',
-        '@angular/cli': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
-        '@angular/compiler-cli': '"~11.0.0 || >=11.0.0-rc.1 <11.0.0"',
+        '@angular-devkit/build-angular': '~0.1100.0',
+        '@angular-devkit/build-ng-packagr': '~0.1100.0',
+        '@angular/cli': '~11.0.0',
+        '@angular/compiler-cli': '~11.0.0',
         '@types/node': '^12.11.1',
         codelyzer: '^6.0.0',
         'ts-node': '~8.3.0',
