41926f94244e4b6ababe6d2186b4362e802f0d75
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-02-10 20:47:50 +0100

    feat: support Angular 11.2.x (#12)

--- __tests__/package-json-versions/v11_1-all-override.ts
+++ __tests__/package-json-versions/v11_1-all-override.ts
@@ -46,7 +46,7 @@ export const packageJsonWithV11_1All = {
   },
   devDependencies: {
     '@angular-devkit/build-angular': '~0.1101.0',
-    '@angular-devkit/build-ng-packagr': '~0.1002.1',
+    '@angular-devkit/build-ng-packagr': '~0.1002.2',
     '@angular-devkit/schematics-cli': '~0.1101.0',
     '@angular/cli': '~11.1.0',
     '@angular/compiler-cli': '~11.1.0',
