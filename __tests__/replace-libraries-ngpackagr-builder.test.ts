ac0b54d16f7969830c846180943d68eccaaf966b
Nacho Vazquez <nachovc1410@gmail.com>
2021-04-25 18:16:34 -0300

    test: refactor tests into multiple test cases

--- __tests__/replace-libraries-ngpackagr-builder.test.ts
+++ __tests__/replace-libraries-ngpackagr-builder.test.ts
@@ -33,71 +33,98 @@ const angular10_1AndUpVersions: AngularVersion[] = [
 ];
 
 describe(replaceLibrariesNgPackagrBuilder.name, () => {
-  preAngular10_1Versions.forEach(version => {
-    test(`when using the version ${version} it should replace all libraries ng-packagr builder with ${preAngular10_1NgPackagrBuilder}`, () => {
-      const actualLumberjackAngularJson = replaceLibrariesNgPackagrBuilder(
-        version,
-        lumberjackWithBothNgPackagrBuildersAngularJson
-      );
-      const actualRegularAngularJson = replaceLibrariesNgPackagrBuilder(
-        version,
-        regularAngular10_1AndUpNgPackagrBuilderAngularJson
-      );
+  describe('when using angular versions older than 10.1', () => {
+    preAngular10_1Versions.forEach(version => {
+      describe(`using the version ${version}`, () => {
+        const actualLumberjackAngularJson = replaceLibrariesNgPackagrBuilder(
+          version,
+          lumberjackWithBothNgPackagrBuildersAngularJson
+        );
+        const actualRegularAngularJson = replaceLibrariesNgPackagrBuilder(
+          version,
+          regularAngular10_1AndUpNgPackagrBuilderAngularJson
+        );
 
-      // lumberjack angular.json
-      expect(actualLumberjackAngularJson).toEqual(
-        lumberjackPreAngular10_1NgPackagrBuilderAngularJson
-      );
-      expect(
-        actualLumberjackAngularJson.projects['ngworker-lumberjack'].architect
-          .buildLib.builder
-      ).toBe(preAngular10_1NgPackagrBuilder);
-      expect(
-        actualLumberjackAngularJson.projects['internal-test-util'].architect
-          .build.builder
-      ).toBe(preAngular10_1NgPackagrBuilder);
+        describe('the lumberjack workspace angular.json', () => {
+          test('should exactly match the snapshot', () => {
+            expect(actualLumberjackAngularJson).toEqual(
+              lumberjackPreAngular10_1NgPackagrBuilderAngularJson
+            );
+          });
 
-      //regular application angular.json
-      expect(actualRegularAngularJson).toEqual(
-        regularPreAngular10_1NgPackagrBuilderAngularJson
-      );
-      expect(
-        actualRegularAngularJson.projects['angular-lib'].architect.build.builder
-      ).toBe(preAngular10_1NgPackagrBuilder);
+          test('should change the ng-packagr library builders', () => {
+            expect(
+              actualLumberjackAngularJson.projects['ngworker-lumberjack']
+                .architect.buildLib.builder
+            ).toBe(preAngular10_1NgPackagrBuilder);
+            expect(
+              actualLumberjackAngularJson.projects['internal-test-util']
+                .architect.build.builder
+            ).toBe(preAngular10_1NgPackagrBuilder);
+          });
+        });
+
+        describe('a regular angular workspace angular.json', () => {
+          test('should exactly match the snapshot', () => {
+            expect(actualRegularAngularJson).toEqual(
+              regularPreAngular10_1NgPackagrBuilderAngularJson
+            );
+          });
+          test('should change the ng-packagr library builders', () => {
+            expect(
+              actualRegularAngularJson.projects['angular-lib'].architect.build
+                .builder
+            ).toBe(preAngular10_1NgPackagrBuilder);
+          });
+        });
+      });
     });
   });
+  describe('when using angular versions newer or equal than 10.1', () => {
+    angular10_1AndUpVersions.forEach(version => {
+      describe(`using the version ${version}`, () => {
+        const actualLumberjackAngularJson = replaceLibrariesNgPackagrBuilder(
+          version,
+          lumberjackWithBothNgPackagrBuildersAngularJson
+        );
+        const actualRegularAngularJson = replaceLibrariesNgPackagrBuilder(
+          version,
+          regularPreAngular10_1NgPackagrBuilderAngularJson
+        );
 
-  angular10_1AndUpVersions.forEach(version => {
-    test(`when using the version ${version} it should replace all libraries ng-packagr builder with ${angular10_1AndUpNgPackagrBuilder}`, () => {
-      const actualLumberjackAngularJson = replaceLibrariesNgPackagrBuilder(
-        version,
-        lumberjackWithBothNgPackagrBuildersAngularJson
-      );
-      const actualRegularAngularJson = replaceLibrariesNgPackagrBuilder(
-        version,
-        regularPreAngular10_1NgPackagrBuilderAngularJson
-      );
+        describe('the lumberjack workspace angular.json', () => {
+          test('should exactly match the snapshot', () => {
+            expect(actualLumberjackAngularJson).toEqual(
+              lumberjack10_1AndUpNgPackagrBuilderAngularJson
+            );
+          });
 
-      // lumberjack angular.json
-      expect(actualLumberjackAngularJson).toEqual(
-        lumberjack10_1AndUpNgPackagrBuilderAngularJson
-      );
-      expect(
-        actualLumberjackAngularJson.projects['ngworker-lumberjack'].architect
-          .buildLib.builder
-      ).toBe(angular10_1AndUpNgPackagrBuilder);
-      expect(
-        actualLumberjackAngularJson.projects['internal-test-util'].architect
-          .build.builder
-      ).toBe(angular10_1AndUpNgPackagrBuilder);
+          test('should change the ng-packagr library builders', () => {
+            expect(
+              actualLumberjackAngularJson.projects['ngworker-lumberjack']
+                .architect.buildLib.builder
+            ).toBe(angular10_1AndUpNgPackagrBuilder);
+            expect(
+              actualLumberjackAngularJson.projects['internal-test-util']
+                .architect.build.builder
+            ).toBe(angular10_1AndUpNgPackagrBuilder);
+          });
+        });
 
-      //regular application angular.json
-      expect(actualRegularAngularJson).toEqual(
-        regularAngular10_1AndUpNgPackagrBuilderAngularJson
-      );
-      expect(
-        actualRegularAngularJson.projects['angular-lib'].architect.build.builder
-      ).toBe(angular10_1AndUpNgPackagrBuilder);
+        describe('a regular angular workspace angular.json', () => {
+          test('should exactly match the snapshot', () => {
+            expect(actualRegularAngularJson).toEqual(
+              regularAngular10_1AndUpNgPackagrBuilderAngularJson
+            );
+          });
+          test('should change the ng-packagr library builders', () => {
+            expect(
+              actualRegularAngularJson.projects['angular-lib'].architect.build
+                .builder
+            ).toBe(angular10_1AndUpNgPackagrBuilder);
+          });
+        });
+      });
     });
   });
 });
